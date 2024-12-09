f48a259b59360565f64814e7f946fcc0b9deeb28
Azzaldeen98 <azzaldeen771211417@gmail.com>
2024-12-09 21:21:23 +0300

    AZM commit 5

---
+++ Infrastructure/Repository/Auth/AuthRepository.cs
@@ -0,0 +1,29 @@
+﻿
+using AutoMapper;
+using Domain.Entities.Plans;
+using Domain.Repository.Auth;
+using Domain.Repository.Plans;
+using Infrastructure.DataSource.Seeds;
+using Infrastructure.Models.Plans;
+using Shared.Settings;
+
+namespace Infrastructure.Repository.Plans
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly SeedsPlans seedsPlans;
+        private readonly IMapper _mapper;
+        private readonly ApplicationModeService appModeService;
+        public AuthRepository(IMapper mapper, ApplicationModeService appModeService)
+        {
+
+            seedsPlans = new SeedsPlans();
+            _mapper = mapper;
+            this.appModeService = appModeService;
+        }
+
+
+
+
+    }
+}
